75d3e10c33e7c31d26797c5f02ee71acdb07e267
ErnisKasumi <119933258+ErnisKasumi@users.noreply.github.com>
2023-05-14 19:08:28 +0200

    Added a new branch
    new branch

---
+++ react/oxygenquiz/src/components/Navbar/index.jsx
@@ -0,0 +1,36 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import OxygenIcon from '../../fotot/OxygenIcon.png'
+import { Button } from '@mui/material';
+import '@mui/material'
+import { Avatar } from '@mui/material';
+ '../../npm/node_modules/bootstrap/dist/css/bootstrap.css'
+import LeaderboardIcon from '@mui/icons-material/Leaderboard';
+import { useNavigate } from 'react-router-dom';
+
+const Navbar = () => {
+
+  
+  const navigate = useNavigate();
+
+
+return (
+
+<nav className="navbar">
+  <div className="navbar__logo">
+    <img src={OxygenIcon} />
+  </div>
+  <div className="navbar__links">
+    <NavLink activeclassname="active" to="/">Ballina</NavLink>
+    <NavLink activeclassname="active" to="/leaderboard">Leaderboard</NavLink>
+    <NavLink activeclassname="active" to="/components/Profile"><Avatar alt="Kastriot"  /></NavLink>
+    
+ <Button onClick={() => navigate('/components/Login')} variant='contained' size='large'>Login</Button>
+    
+  </div>
+</nav>
+)
+
+}
+
+export default Navbar
